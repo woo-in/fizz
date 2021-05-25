4710043185abfb3d8b55aa3c0ea5a5e8fea348d7
woo-in <69314509+woo-in@users.noreply.github.com>
2021-05-25 21:14:25 +0900

    Update Color.js

--- Color.js
+++ Color.js
@@ -2,28 +2,21 @@ var ColorControler = { }; // 객체 이용
 
 
 ColorControler.LinkSetColor = function(color) {
-  var array = document.querySelectorAll('.notnow');
-
-  var i=0;
-  while(i<array.length){
-
-    array[i].style.color = color;
-    i=i+1;
-  }
+    $('.notnow').css('color',color);
 }
 
 ColorControler.BodySetColor = function(color){
-  document.querySelector('body').style.color = color;
+    $('body').css('color',color);
 }
 
 ColorControler.BodySetBackColor = function(color)
 {
-  document.querySelector('body').style.backgroundColor = color;
+  $('body').css('backgroundColor',color);
 }
 
 ColorControler.TitleSetColor= function(color)
 {
-  document.querySelector('#title1').style.color = color;
+  $('#title1').css('color',color);
 }
 
 function NightDayHander(InputB) {
